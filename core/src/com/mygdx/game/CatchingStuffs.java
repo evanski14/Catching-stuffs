b6e891fe225066c06e3a951fcf3f63ffd2382127
evanski14 <killmasterjoy13@live.com>
2017-12-17 07:07:15 +0100

    add sound

--- core/src/com/mygdx/game/CatchingStuffs.java
+++ core/src/com/mygdx/game/CatchingStuffs.java
@@ -6,8 +6,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
@@ -19,9 +22,20 @@ public class CatchingStuffs extends ApplicationAdapter {
 	private Texture pepeImg;
 	private Texture bucketImg;
 	private Music bgMusic;
+	private Sound roblox;
+	private Sound WOW;
+	private Sound Triple;
+	private Sound GetTheCam;
+	private Sound Dota;
 	private Rectangle bucket;
 	private Array<Rectangle> pepes;
 	private long lastDropTime;
+	private int score;
+	private String ScoreName;
+	BitmapFont ScoreBitmapFontName;
+	private int streak;
+	private String StreakName;
+	BitmapFont StreakBitmapFontName;
 	
 	//randome spawn pepe
 	private void pepeSpawn() {
@@ -47,6 +61,13 @@ public class CatchingStuffs extends ApplicationAdapter {
 		bgMusic = Gdx.audio.newMusic(Gdx.files.internal("Gold_Rush.mp3"));
 		bgMusic.setLooping(true);
 		bgMusic.play();
+		bgMusic.setVolume(0.1f);
+		
+		roblox = Gdx.audio.newSound(Gdx.files.internal("roblox.mp3"));
+		WOW = Gdx.audio.newSound(Gdx.files.internal("WOW.mp3"));
+		Triple = Gdx.audio.newSound(Gdx.files.internal("Triple.wav"));
+		GetTheCam = Gdx.audio.newSound(Gdx.files.internal("GetTheCam.wav"));
+		Dota = Gdx.audio.newSound(Gdx.files.internal("Dota.mp3"));
 		
 		//actually create stuffs
 			//bucket
@@ -59,6 +80,16 @@ public class CatchingStuffs extends ApplicationAdapter {
 		pepes = new Array<Rectangle>();
 		pepeSpawn();
 		
+		//ScoreBoard
+		score = 0;
+		ScoreName = "score: 0";
+		ScoreBitmapFontName = new BitmapFont();
+		
+		//Streak
+		streak = 0;
+		StreakName = "streak: 0";
+		StreakBitmapFontName = new BitmapFont();
+		
 	}
 
 	@Override
@@ -66,6 +97,12 @@ public class CatchingStuffs extends ApplicationAdapter {
 		Gdx.gl.glClearColor(0, 1, 0.1f, 10);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
+		//draw scoreboard
+		ScoreBitmapFontName.setColor(Color.BLACK);
+		ScoreBitmapFontName.draw(batch, ScoreName, 675, 575);
+		//draw streakboard
+		StreakBitmapFontName.setColor(Color.BLACK);
+		StreakBitmapFontName.draw(batch, StreakName, 675, 550);
 		//draw bucket
 		batch.draw(bucketImg, bucket.x, bucket.y);
 		//draw pepe
@@ -104,7 +141,27 @@ public class CatchingStuffs extends ApplicationAdapter {
 				iter.remove();
 			}
 			if(pepe.overlaps(bucket)) {
+				score++;
+				ScoreName = "score: " + score;
+				streak++;
+				if(streak == 3) {
+					Triple.play(0.2f);
+				}
+				if(score == 20) {
+					GetTheCam.play(0.2f);
+				}
+				if(streak == 10) {
+					Dota.play();
+				}
+				StreakName = "streak: " + streak;
 				iter.remove();
+				WOW.play();
+			}
+			if(pepe.y < 0) {
+				streak = 0;
+				StreakName = "streal: " + streak;
+				iter.remove();
+				roblox.play();
 			}
 		}
 	}
